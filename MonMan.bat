22c654a81ecc0138c045a20f7328d5e9fe1d306f
Ethan <dusty3692@gmail.com>
2025-06-21 15:21:40 +0100

    Initial commit with basic no-output ps1 file

---
+++ MonMan.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Define the PowerShell command
+set PS_COMMAND="C:\Users\Ethan\Desktop\TVMON\MonMan.ps1"
+
+:: Run the PowerShell command
+powershell -Command "%PS_COMMAND%"
---
+++ MonMan.ps1
@@ -0,0 +1,7 @@
+﻿$tvmonOB = Get-DisplayInfo | Where-Object -Property DisplayName -EQ 'SAMSUNG'
+#$othermonsOB = Get-DisplayInfo | Where-Object -Property DisplayName -EQ "PL2280H"
+if ($tvmonOB.Active){
+    Disable-Display $tvmonOB.DisplayId
+}else {
+    Enable-Display $tvmonOB.DisplayId
+}
